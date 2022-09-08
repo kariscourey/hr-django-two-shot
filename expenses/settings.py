e82ba5b974f74ed439915c30bb1bec96298f1579
karis courey <kariscourey@gmail.com>
2022-09-08 12:53:44 -0400

    feat 3 complete

--- expenses/settings.py
+++ expenses/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
+    "receipts.apps.ReceiptsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
